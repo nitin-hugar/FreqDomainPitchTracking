863e7f00f461f94b720304b184a9bc1c811ff367
medioqrity <medioqrity@gmail.com>
2021-11-05 01:09:52 -0400

    add solution of section D; simplify c1 & c2

--- ass3solution.py
+++ ass3solution.py
@@ -8,7 +8,7 @@ from glob import glob
 
 T = np.ndarray  # for autocomplete engine, will be deleted before submission
 BLOCK_SIZE = 1 << 10
-HOP_SIZE = BLOCK_SIZE >> 4
+HOP_SIZE = BLOCK_SIZE >> 2
 DB_TRUNCATION_THRESHOLD = -100
 
 
@@ -102,7 +102,7 @@ def compute_spectrogram(xb: T, fs: int):
     return Xb, f_in_hz
 
 
-# -- A.2 ---
+# --- A.2 ---
 def track_pitch_fftmax(x: T, blockSize: int, hopSize: int, fs: int):
     xb, time_in_sec = block_audio(x, blockSize, hopSize, fs)
     Xb, f_in_hz = compute_spectrogram(xb, fs)
@@ -110,18 +110,18 @@ def track_pitch_fftmax(x: T, blockSize: int, hopSize: int, fs: int):
     return f0, time_in_sec  # both have shape (num_of_block, )
 
 
-# -- B.1 ---
+# --- B.1 ---
 def get_f0_from_Hps(X, fs, order):
     hps = X.copy()
     for i in range(1, order):
         Xd = X[:, :: i + 1]
         hps = hps[:, : Xd.shape[1]]
         hps *= Xd
-    f0 = f_in_hz[np.argmax(hps, axis=-1)]
+    f0 = f_in_hz[np.argmax(hps, axis=-1)]  # f_in_hz is outside variable
     return f0
 
 
-# -- B.2 ---
+# --- B.2 ---
 def track_pitch_hps(x, blockSize, hopSize, fs):
     xb, time_in_sec = block_audio(
         x, blockSize=blockSize, hopSize=hopSize, fs=fs
@@ -133,31 +133,50 @@ def track_pitch_hps(x, blockSize, hopSize, fs):
 
 # --- C.1 ---
 def extract_rms(xb):
-    rms = np.zeros(xb.shape[0])
-    for block in range(xb.shape[0]):
-        rms[block] = np.sqrt(np.mean(np.square(xb[block])))
-        threshold = 1e-5  # truncated at -100dB
-        if rms[block] < threshold:
-            rms[block] = threshold
-        rms[block] = 20 * np.log10(rms[block])
+    rms = np.maximum(
+        20 * np.log10(np.sqrt(np.mean(xb ** 2, axis=-1))),
+        DB_TRUNCATION_THRESHOLD,
+    )
     return rms
 
 
 # --- C.2 ---
 def create_voicing_mask(rmsDb, thresholdDb):
-    mask = np.zeros(rmsDb.shape[0])
-    for i in range(rmsDb.shape[0]):
-        if rmsDb[i] >= thresholdDb:
-            mask[i] = 1
-        else:
-            mask[i] = 0
-    return mask
+    return 0 + rmsDb >= thresholdDb
 
 
-# -- C.3 --
+# --- C.3 ---
 def apply_voicing_mask(f0, mask):
     f0Adj = f0 * mask
     return f0Adj
+
+
+# --- D.1 ---
+def eval_voiced_fp(estimation, annotation):
+    # false positive: Actually negative, predicted positive
+    return np.sum(estimation != 0) / np.sum(annotation == 0)
+
+
+# --- D.2 ---
+def eval_voiced_fn(estimation, annotation):
+    # false negative
+    return np.sum(estimation == 0) / np.sum(annotation != 0)
+
+
+# --- D.3 ---
+def eval_pitchtrack_v2(estimation, annotation):
+    # return RMS of cent
+    err_cent_rms = 0
+    for a, b in zip(estimation, annotation):
+        if a and b:
+            err_cent_rms += (1200 * np.log2(a / b)) ** 2
+    err_cent_rms = (err_cent_rms / len(estimation)) ** 0.5
+    pfp = eval_voiced_fp(estimation, annotation)
+    pfn = eval_voiced_fn(estimation, annotation)
+    return err_cent_rms, pfp, pfn
+
+
+# --- E.1 ---
 
 
 if __name__ == "__main__":
@@ -167,8 +186,15 @@ if __name__ == "__main__":
         fs, x = tool_read_audio(full_filename)
         xb, time_in_sec = block_audio(x, BLOCK_SIZE, HOP_SIZE, fs)
         Xb, f_in_hz = compute_spectrogram(xb, fs)
-        f0, _ = track_pitch_fftmax(x, BLOCK_SIZE, HOP_SIZE, fs)
-        print(Xb.shape, f0.shape)
+        f0_fftmax, _ = track_pitch_fftmax(x, BLOCK_SIZE, HOP_SIZE, fs)
+        f0_hps, _ = track_pitch_hps(x, BLOCK_SIZE, HOP_SIZE, fs)
+        rms = extract_rms(xb)
+        voicing_mask = create_voicing_mask(rms, -30)
+        plt.plot(time_in_sec, rms)
+        plt.plot(time_in_sec, voicing_mask)
+        plt.show()
+        plt.clf()
+        print(Xb.shape, f0_fftmax.shape, f0_hps.shape)
 
         plt.figure(figsize=(36, 6))
         # plt.subplot(2, 1, 1)
@@ -178,7 +204,8 @@ if __name__ == "__main__":
             origin="lower",
             extent=[0, time_in_sec[-1], 0, fs / 2],
         )
-        plt.plot(time_in_sec, f0, label="estimated f0")
+        plt.plot(time_in_sec, f0_fftmax, label="estimated f0 (fft max)")
+        plt.plot(time_in_sec, f0_hps, label="estimated f0 (hps)")
         plt.ylim(bottom=10)
         plt.yscale("log")
         plt.ylabel("Magnitude Response [Hz]")
